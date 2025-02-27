99e0499661a0f20b57d7ebf114f84bdb043e4b15
Kacper Agatowski <n00212272@iadt.ie>
2025-02-27 22:48:45 +0000

    changed schema to match the model

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,14 +1,23 @@
 from pydantic import BaseModel
 
 class UserCreate(BaseModel):
-    name: str
+    first_name: str  
+    last_name: str   
     email: str
+    mobile_number: str
+    password: str  
+
+    class Config:
+        from_attributes = True 
 
 class UserResponse(BaseModel):
     id: int
-    name: str
+    first_name: str  
+    last_name: str   
     email: str
+    mobile_number: str
+    day_off_count: int 
+    team_id: int | None  
 
     class Config:
         from_attributes = True
-
